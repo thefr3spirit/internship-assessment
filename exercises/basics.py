8ded705d32986675bde7d8848192951c7c88cdbb
thefr3spirit <beofr3spirit@gmail.com>
2026-05-12 10:43:54 +0300

    Finished Part 1 by completing the 2 functions. and passing all 5 tests

--- exercises/basics.py
+++ exercises/basics.py
@@ -12,7 +12,20 @@ def collatz(n: int) -> List[int]:
     For example, if n = 3, the sequence of values is: 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
     So, your function would return: [3, 10, 5, 16, 8, 4, 2, 1]
     """
-    pass
+    
+    #create and start the sequence with the initial int
+    sequence = [n]
+
+    while n != 1:
+        if n%2 == 0:
+            n = n/2
+        else:
+            n = n*3+1
+        sequence.append(n)
+
+    return sequence            
+
+        
 
 
 def distinct_numbers(numbers: List[int]) -> int:
@@ -21,4 +34,7 @@ def distinct_numbers(numbers: List[int]) -> int:
 
     E.g if numbers = [2, 3, 2, 2, 3], then the answer is 2 since there are only 2 unique numbers: 2 and 3.
     """
-    pass
+    
+    #use the set function to create a list with no duplicates
+    unique_list = set(numbers)
+    return len(unique_list)
